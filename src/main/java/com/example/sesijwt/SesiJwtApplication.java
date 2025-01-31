58f6c24e0d7d5f84d9e6a57153125d6f22e52e72
JoaoNascimento1802 <joao.emanuel.pn@gmail.com>
2025-01-30 21:21:06 -0300

    :wrench: chore: First code

---
+++ src/main/java/com/example/sesijwt/SesiJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sesijwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesiJwtApplication.class, args);
+	}
+
+}
